88d12998b2d8737f649bdc8b1bd4bbbd16550dbc
Alexander Strizhakov <alex.d3vils@gmail.com>
2022-09-10 11:54:42 +0400

    formatting

--- test/flow_timer/focus_sessions_test.exs
+++ test/flow_timer/focus_sessions_test.exs
@@ -23,7 +23,9 @@ defmodule FlowTimer.FocusSessionsTest do
     test "create_focus_session/1 with valid data creates a focus_session" do
       valid_attrs = %{finished_at: ~N[2022-09-09 07:14:00], number: 42}
 
-      assert {:ok, %FocusSession{} = focus_session} = FocusSessions.create_focus_session(valid_attrs)
+      assert {:ok, %FocusSession{} = focus_session} =
+               FocusSessions.create_focus_session(valid_attrs)
+
       assert focus_session.finished_at == ~N[2022-09-09 07:14:00]
       assert focus_session.number == 42
     end
@@ -36,21 +38,29 @@ defmodule FlowTimer.FocusSessionsTest do
       focus_session = focus_session_fixture()
       update_attrs = %{finished_at: ~N[2022-09-10 07:14:00], number: 43}
 
-      assert {:ok, %FocusSession{} = focus_session} = FocusSessions.update_focus_session(focus_session, update_attrs)
+      assert {:ok, %FocusSession{} = focus_session} =
+               FocusSessions.update_focus_session(focus_session, update_attrs)
+
       assert focus_session.finished_at == ~N[2022-09-10 07:14:00]
       assert focus_session.number == 43
     end
 
     test "update_focus_session/2 with invalid data returns error changeset" do
       focus_session = focus_session_fixture()
-      assert {:error, %Ecto.Changeset{}} = FocusSessions.update_focus_session(focus_session, @invalid_attrs)
+
+      assert {:error, %Ecto.Changeset{}} =
+               FocusSessions.update_focus_session(focus_session, @invalid_attrs)
+
       assert focus_session == FocusSessions.get_focus_session!(focus_session.id)
     end
 
     test "delete_focus_session/1 deletes the focus_session" do
       focus_session = focus_session_fixture()
       assert {:ok, %FocusSession{}} = FocusSessions.delete_focus_session(focus_session)
-      assert_raise Ecto.NoResultsError, fn -> FocusSessions.get_focus_session!(focus_session.id) end
+
+      assert_raise Ecto.NoResultsError, fn ->
+        FocusSessions.get_focus_session!(focus_session.id)
+      end
     end
 
     test "change_focus_session/1 returns a focus_session changeset" do
